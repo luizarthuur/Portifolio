2cffa428eed9c7027bdca5020f39942e46206420
luizarthuur <luizarthurwinter@gmail.com>
2026-03-18 15:56:04 -0300

    adicao de conteudo da quinta secao

--- luiz-arthur-portifolio/src/components/5th-section/index.tsx
+++ luiz-arthur-portifolio/src/components/5th-section/index.tsx
@@ -3,23 +3,34 @@ import './styles.css';
 
 const freelances = [
   {
-    link: "https://hernaneyewear.com.br/",
+    link: "https://hernaneyewear.com.br/quem-usa",
     linkLabel: "Veja o website",
     image: "/public/mock-hernan-eyewear.png",
     alt: "Hernan Eyewear",
+    text: "Landing page desenvolvida para mostrar quem utiliza os produtos da marca Hernan Eyewear"
   },
   {
-    link: "https://freelance2.com",
+    link: "https://cesarreboques.com/",
     linkLabel: "Veja o website",
-    image: "/public/mock-freelance-2.png",
-    alt: "Freelance 2",
+    image: "/public/mock-cesar-resgates.png",
+    alt: "Cesar Resgates",
+    text: "Criação de website para o Cesar Resgates, empresa de resgate de motocicletas, desenvolvimento de website e landing page com WordPress e Elementor, com foco em comunicação clara, design mobile-first e boas práticas de SEO para geração de leads."
   },
   {
-    link: "https://freelance3.com",
+    link: "https://hernaneyewear.com.br",
     linkLabel: "Veja o website",
-    image: "/public/mock-freelance-3.png",
-    alt: "Freelance 3",
+    image: "/public/mock-hernan-eyewear-2.png",
+    alt: "Hernan Eyewear",
+    text: "Carrossel dinâmico integrado ao GTM e modal de escolha de lentes na página de produto, melhorando a experiência na jornada de compra do usuário"
   },
+  {
+    link: "https://gabriela-costa-art.wasmer.app/index.php/elementor-10/",
+    linkLabel: "Veja o website",
+    image: "/public/mock-gabriela.png",
+    alt: "Cesar Resgates",
+    text: "Criação de landing page para artista visual — estúdio de design e portfólio de pinturas, desenvolvimento com WordPress e Elementor, priorizando apresentação estética, navegação fluida e layout mobile-first que reflete a identidade criativa da artista — transformando a própria página em uma extensão da sua arte."
+  },
+  
 ];
 
 const ArrowIcon = () => (
@@ -79,6 +90,8 @@ export default function FifthSection() {
                 alt={freelance.alt}
                 className="carousel-image fourth-section-project-image"
               />
+              <p className="carousel-title-text">O que foi feito neste projeto?</p>
+              <p className="carousel-card-text">{freelance.text}</p>
             </div>
 
             <button className="carousel-arrow carousel-arrow-right" onClick={next} aria-label="Próximo freelance">
